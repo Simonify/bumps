b756a706fb34fc32f4850dbf0147fffb7b1591be
Simon Fletcher <simon@wakecodesleep.com>
2015-09-15 02:34:41 -0700

    Better demo.

--- examples/example.js
+++ examples/example.js
@@ -1,57 +1,57 @@
 export default {
-    "id": "bump1",
-    "name": "PSA",
-    "order": ["segment-1", "segment-2", "segment-3", "segment-pepe", "segment-4", "segment-5"],
+    "id": "demo",
+    "name": "Example bump",
+    "order": ["segment-1", "segment-2", "segment-pepe", "segment-3", "segment-4", "segment-5"],
     "segments": {
         "segment-1": {
             "id": "segment-1",
             "type": "TEXT",
-            "duration": 2.2,
+            "duration": 2.1,
             "label": "Text 1",
-            "text": "Attention."
+            "text": "Welcome!"
         },
         "segment-2": {
             "id": "segment-2",
             "type": "TEXT",
-            "duration": 2.9,
+            "duration": 1.2,
             "label": "Text 2",
-            "text": "We have an important PSA."
+            "text": "Thanks for trying out the TTN bump-maker!"
         },
         "segment-3": {
             "id": "segment-3",
             "type": "TEXT",
-            "duration": 1,
+            "duration": 1.5,
             "label": "Text 3",
-            "text": "We have a rare pepe for sale"
+            "text": "It's pretty early in development"
         },
         "segment-pepe": {
             "id": "segment-pepe",
             "type": "IMAGE",
-            "duration": 1.8,
+            "duration": 1.2,
             "label": "Rare Pepe",
-            "url": "https://i.imgur.com/pEMGBhR.jpg"
+            "url": "http://i.imgur.com/zC5bcAZ.gif"
         },
         "segment-4": {
             "id": "segment-4",
             "type": "TEXT",
-            "duration": 2.8,
+            "duration": 1.2,
             "label": "Text 4",
-            "text": "Bidding starts at $50"
+            "text": "but please feel free to play around."
         },
         "segment-5": {
             "id": "segment-5",
             "type": "LOGO",
-            "duration": 2,
+            "duration": 2.1,
             "label": "Logo"
         }
     },
-    "duration": 12.8,
+    "duration": 9.3,
     "audio": {
         "id": "youtube-audio",
         "type": "YOUTUBE",
-        "url": "https://www.youtube.com/watch?v=l7u9hP4r1S8",
-        "start": 0,
-        "duration": 12.8,
+        "url": "https://www.youtube.com/watch?v=xBQv6hmZM80",
+        "start": 20,
+        "duration": 9.3,
         "label": "Audio"
     }
 };
